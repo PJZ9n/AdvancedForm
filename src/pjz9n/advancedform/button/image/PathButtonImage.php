6c5606347e4d752d1430cbb5f7477bebe4e99ff6
PJZ9n <38120936+PJZ9n@users.noreply.github.com>
2022-07-11 20:14:15 +0900

    ButtonImage: Add implementations for path and url

---
+++ src/pjz9n/advancedform/button/image/PathButtonImage.php
@@ -0,0 +1,38 @@
+<?php
+
+/*
+ * Copyright (c) 2022 PJZ9n.
+ *
+ * This file is part of AdvancedForm.
+ *
+ * AdvancedForm is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * AdvancedForm is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with AdvancedForm. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+declare(strict_types=1);
+
+namespace pjz9n\advancedform\button\image;
+
+/**
+ * Get an image from a resource pack
+ */
+class PathButtonImage extends ButtonImage
+{
+    /**
+     * @param string $path Path on the resource pack (For example "textures/items/apple")
+     */
+    public function __construct(string $path)
+    {
+        parent::__construct(ButtonImageTypes::PATH, $path);
+    }
+}
