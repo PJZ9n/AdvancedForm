40af30e10f9538cff9f4caf69d0b3a91bd322af0
PJZ9n <38120936+PJZ9n@users.noreply.github.com>
2022-07-12 06:56:37 +0900

    Implements ModalForm

---
+++ src/pjz9n/advancedform/modal/CallbackModalForm.php
@@ -0,0 +1,79 @@
+<?php
+
+/*
+ * Copyright (c) 2022 PJZ9n.
+ *
+ * This file is part of AdvancedForm.
+ *
+ * AdvancedForm is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * AdvancedForm is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with AdvancedForm. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+declare(strict_types=1);
+
+namespace pjz9n\advancedform\modal;
+
+use Closure;
+use pjz9n\advancedform\modal\response\ModalFormResponse;
+use pocketmine\player\Player;
+use pocketmine\utils\Utils;
+
+class CallbackModalForm extends ModalForm
+{
+    /**
+     * @param string $title Form title
+     * @param string $text Message text to display on the form
+     * @param Closure|null $handleSelect Called when the button is selected
+     * @phpstan-param Closure(Player, ModalFormResponse): void|null $handleSelect
+     *
+     * @see ModalForm::handleSelect()
+     */
+    public function __construct(
+        string             $title,
+        string             $text,
+        protected ?Closure $handleSelect = null,
+    )
+    {
+        if ($this->handleSelect !== null) {
+            // @formatter:off
+            Utils::validateCallableSignature(function (Player $player, ModalFormResponse $response): void {}, $this->handleSelect);
+            // @formatter:on
+        }
+
+        parent::__construct($title, $text);
+    }
+
+    /**
+     * @phpstan-return Closure(Player, ModalFormResponse): void|null
+     */
+    public function getHandleSelect(): ?Closure
+    {
+        return $this->handleSelect;
+    }
+
+    /**
+     * @phpstan-param Closure(Player, ModalFormResponse): void|null $handleSelect
+     */
+    public function setHandleSelect(?Closure $handleSelect): self
+    {
+        $this->handleSelect = $handleSelect;
+        return clone $this;
+    }
+
+    protected function handleSelect(Player $player, ModalFormResponse $response): void
+    {
+        if ($this->handleSelect !== null) {
+            ($this->handleSelect)($player, $response);
+        }
+    }
+}
---
+++ src/pjz9n/advancedform/modal/ModalForm.php
@@ -0,0 +1,128 @@
+<?php
+
+/*
+ * Copyright (c) 2022 PJZ9n.
+ *
+ * This file is part of AdvancedForm.
+ *
+ * AdvancedForm is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * AdvancedForm is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with AdvancedForm. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+declare(strict_types=1);
+
+namespace pjz9n\advancedform\modal;
+
+use pjz9n\advancedform\button\Button;
+use pjz9n\advancedform\FormBase;
+use pjz9n\advancedform\FormTypes;
+use pjz9n\advancedform\modal\response\ModalFormResponse;
+use pocketmine\form\FormValidationException;
+use pocketmine\player\Player;
+use function gettype;
+use function is_bool;
+
+class ModalForm extends FormBase
+{
+    static protected function getType(): string
+    {
+        return FormTypes::MODAL;
+    }
+
+    protected Button $yesButton;
+    protected Button $noButton;
+
+    /**
+     * @param string $title Form title
+     * @param string $text Message text to display on the form
+     */
+    public function __construct(
+        string           $title,
+        protected string $text,
+        ?Button          $yesButton = null,
+        ?Button          $noButton = null,
+    )
+    {
+        $this->yesButton = $yesButton ?? new Button("gui.yes");
+        $this->noButton = $noButton ?? new Button("gui.no");
+
+        parent::__construct($title);
+    }
+
+    public function getText(): string
+    {
+        return $this->text;
+    }
+
+    public function setText(string $text): self
+    {
+        $this->text = $text;
+        return clone $this;
+    }
+
+    public function getYesButton(): Button
+    {
+        return $this->yesButton;
+    }
+
+    public function setYesButton(Button $yesButton): self
+    {
+        $this->yesButton = $yesButton;
+        return clone $this;
+    }
+
+    public function getNoButton(): Button
+    {
+        return $this->noButton;
+    }
+
+    public function setNoButton(Button $noButton): self
+    {
+        $this->noButton = $noButton;
+        return clone $this;
+    }
+
+    final public function handleResponse(Player $player, $data): void
+    {
+        if (!is_bool($data)) {
+            throw new FormValidationException("Excepted bool, got " . gettype($data));
+        }
+        $selectedButton = $data ? $this->yesButton : $this->noButton;
+        $handler = $selectedButton->getHandler();
+        if ($handler === null || (!$handler->handle($this, $selectedButton, $player))) {
+            $this->handleSelect($player, new ModalFormResponse($selectedButton, $data));
+        }
+    }
+
+    /**
+     * Called when the button is selected
+     * NOTE: This will not be called if successfully processed by ButtonHandler!
+     */
+    protected function handleSelect(Player $player, ModalFormResponse $response): void
+    {
+        //NOOP
+    }
+
+    /**
+     * @return string[]
+     * @phpstan-return array<string, string>
+     */
+    protected function getAdditionalData(): array
+    {
+        return [
+            "content" => $this->text,
+            "button1" => $this->yesButton->getText(),
+            "button2" => $this->noButton->getText(),
+        ];
+    }
+}
